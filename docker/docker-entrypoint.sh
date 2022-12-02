8bc0e1aee2eef1c9d837af4f4fbde6aefeb9e3e0
Matthew B White <mbwhite@users.noreply.github.com>
2022-12-02 10:42:57 +0000

    Update to include maven and gradle (#126)
    Add support to permit user id to be specificed
    
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>
    
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,6 +3,19 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 set -euo pipefail
+
+# If this image is run with -u <random UID>, as happens on Red Hat OpenShift, then
+# the user is not in the /etc/passwd file. This causes Ansible to fail, so we need
+# to add the user to /etc/passwd now before Ansible runs.
+if ! whoami &> /dev/null; then
+    sed '/microfab/d' /etc/passwd > /tmp/passwd
+    cat /tmp/passwd > /etc/passwd
+    rm -f /tmp/passwd
+    echo "microfab:x:$(id -u):0::/home/microfab:/bin/bash" >> /etc/passwd
+    export HOME=/home/microfab
+fi
+
+
 if [ -n "${MICROFAB_CONFIG:-}" ]; then
     COUCHDB_ENABLED=$(echo "${MICROFAB_CONFIG}" | jq -r '. | if has("couchdb") then .couchdb else true end')
     if [ "${COUCHDB_ENABLED}" = "true" ]; then
