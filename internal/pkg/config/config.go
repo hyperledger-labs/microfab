a762636db01748819667324014cbc034d88a8cee
Matthew B White <matthew@mh-white.com>
2023-05-19 14:23:49 +0100

    Correct file header

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -1,13 +1,8 @@
 /*
- * IBM Confidential
- * OCO Source Materials
- *
- * (C) Copyright IBM Corp. 2020  All Rights Reserved.
- *
- * The source code for this program is not published or otherwise
- * divested of its trade secrets, irrespective of what has been
- * deposited with the U.S. Copyright Office.
- */
+Copyright IBM Corp. 2017 All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
 
 package config
 
